073330f40a3da1c23a8df864cb069e7cf1236b32
ADMIN-PC\ADMIN <lokobeleza2010@hotmail.com>
2019-11-30 09:31:50 -0300

    Atualização Routes.

--- node-api-2.0/src/routes.js
+++ node-api-2.0/src/routes.js
@@ -8,45 +8,45 @@ const MotoristaController   = require( './controllers/MotoristaController' ),
       routes                = express.Router( );
 
 /*Motorista*/
-routes.get   ( '/motorista',                MotoristaController.index  );
-routes.get   ( '/motorista/:id',            MotoristaController.show   );
-routes.post  ( '/motorista',                MotoristaController.store  );
-routes.put   ( '/motorista/:id',            MotoristaController.update );
-routes.delete( '/motorista/:id',            MotoristaController.delete );
+routes.get   ( '/motorista',                 MotoristaController.index  );
+routes.get   ( '/motorista/:idMotorista',    MotoristaController.show   );
+routes.post  ( '/motorista',                 MotoristaController.store  );
+routes.put   ( '/motorista/:idMotorista',    MotoristaController.update );
+routes.delete( '/motorista/:idMotorista',    MotoristaController.delete );
 
 /*Veiculo*/
-routes.get   ( '/veiculo',                  VeiculosController.index   );
-routes.get   ( '/veiculo/:id',              VeiculosController.show    );
-routes.post  ( '/veiculo',                  VeiculosController.store   );
-routes.put   ( '/veiculo/:id',              VeiculosController.update  );
-routes.delete( '/veiculo/:id',              VeiculosController.delete  );
+routes.get   ( '/veiculo',                   VeiculosController.index   );
+routes.get   ( '/veiculo/:idVeiculo',        VeiculosController.show    );
+routes.post  ( '/veiculo',                   VeiculosController.store   );
+routes.put   ( '/veiculo/:idVeiculo',        VeiculosController.update  );
+routes.delete( '/veiculo/:idVeiculo',        VeiculosController.delete  );
 
 /*Cliente*/
-routes.get   ( '/cliente',                  ClienteController.index    );
-routes.get   ( '/cliente/:id',              ClienteController.show     );
-routes.post  ( '/cliente',                  ClienteController.store    );
-routes.put   ( '/cliente/:id',              ClienteController.update   );
-routes.delete( '/cliente/:id',              ClienteController.delete   );
+routes.get   ( '/cliente',                   ClienteController.index    );
+routes.get   ( '/cliente/:idCliente',        ClienteController.show     );
+routes.post  ( '/cliente',                   ClienteController.store    );
+routes.put   ( '/cliente/:idCliente',        ClienteController.update   );
+routes.delete( '/cliente/:idCliente',        ClienteController.delete   );
 
 /*Locacao*/
-routes.get   ( '/locacao',                  LocacaoController.index    );
-routes.get   ( '/locacao/:id',              LocacaoController.show     );
-routes.post  ( '/locacao',                  LocacaoController.store    );
-routes.put   ( '/locacao/:id',              LocacaoController.update   );
-routes.delete( '/locacao/:id',              LocacaoController.delete   );
+routes.get   ( '/locacao',                   LocacaoController.index    );
+routes.get   ( '/locacao/:idLocacao',        LocacaoController.show     );
+routes.post  ( '/locacao',                   LocacaoController.store    );
+routes.put   ( '/locacao/:idLocacao',        LocacaoController.update   );
+routes.delete( '/locacao/:idLocacao',        LocacaoController.delete   );
 
 /*Modelo*/
 routes.get   ( '/modelo',                    ModeloController.index   );
-routes.get   ( '/modelo/:id',                ModeloController.show    );
+routes.get   ( '/modelo/:idModelo',          ModeloController.show    );
 routes.post  ( '/modelo',                    ModeloController.store   );
-routes.put   ( '/modelo/:id',                ModeloController.update  );
-routes.delete( '/modelo/:id',                ModeloController.delete  );
+routes.put   ( '/modelo/:idModelo',          ModeloController.update  );
+routes.delete( '/modelo/:idModelo',          ModeloController.delete  );
 
 /*Marca*/
 routes.get   ( '/marca',                     MarcaController.index    );
-routes.get   ( '/marca/:id',                 MarcaController.show     );
+routes.get   ( '/marca/:idMarca',            MarcaController.show     );
 routes.post  ( '/marca',                     MarcaController.store    );
-routes.put   ( '/marca/:id',                 MarcaController.update   );
-routes.delete( '/marca/:id',                 MarcaController.delete   );
+routes.put   ( '/marca/:idMarca',            MarcaController.update   );
+routes.delete( '/marca/:idMarca',            MarcaController.delete   );
 
 module.exports = routes;
